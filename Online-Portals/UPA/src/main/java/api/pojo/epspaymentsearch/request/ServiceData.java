0f2fe644253bc99e749e874718b38d654473e183
Priyanka <priyanka_10@optum.com>
2018-05-23 13:52:16 +0530

    service data was missed, pushing it

---
+++ Online-Portals/UPA/src/main/java/api/pojo/epspaymentsearch/request/ServiceData.java
@@ -0,0 +1,98 @@
+package main.java.api.pojo.epspaymentsearch.request;
+
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * The set of attributes used to debug the service
+ * 			
+ * 
+ * <p>Java class for ServiceData complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ServiceData">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="ApplicationIdentifier" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="Version" minOccurs="0">
+ *           &lt;simpleType>
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *             &lt;/restriction>
+ *           &lt;/simpleType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ServiceData", propOrder = {
+    "applicationIdentifier",
+    "version"
+})
+public class ServiceData {
+
+    @XmlElement(name = "ApplicationIdentifier", required = true)
+    protected String applicationIdentifier;
+    @XmlElement(name = "Version")
+    protected String version;
+
+    /**
+     * Gets the value of the applicationIdentifier property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getApplicationIdentifier() {
+        return applicationIdentifier;
+    }
+
+    /**
+     * Sets the value of the applicationIdentifier property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setApplicationIdentifier(String value) {
+        this.applicationIdentifier = value;
+    }
+
+    /**
+     * Gets the value of the version property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     * Sets the value of the version property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setVersion(String value) {
+        this.version = value;
+    }
+
+}
