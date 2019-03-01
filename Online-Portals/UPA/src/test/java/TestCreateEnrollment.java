36141631447f63b65c15f0ea6b438aa4b410ac97
Priyanka Anand <priyanka_10@optum.com>
2019-03-01 17:36:19 +0530

    resolving conflicts ti merge with master

--- Online-Portals/UPA/src/test/java/TestCreateEnrollment.java
+++ Online-Portals/UPA/src/test/java/TestCreateEnrollment.java
@@ -35,32 +35,6 @@ public class TestCreateEnrollment extends TestBase{
 		registrationPage.clickEnrollNow().selectHowYouHeard(option).verifySurveyTables(option);
 	 }
 	
-	/**
-	 * These below test cases belong to US1202167
-	 * @throws IOException
-	 */
-	
-	@Test(priority=4,description="US1202167--TS001/TS002/TS018/TS023 Validates User Enrollment As Health Organisation") 
-	public void testUserEnrollmentAsHealthOrg() throws IOException
-	{
-		String option="Health plan communication";
-		int excelRow=1;
-		UPARegistrationPage registrationPage = new UPARegistrationPage(testConfig);
-		BeginEnrollment beginEnrollmentPage=  registrationPage.clickEnrollNow();
-		BeginEnrollmentContinue beginEnrollmentContinuePage= beginEnrollmentPage.selectHowYouHeard(option);
-		beginEnrollmentContinuePage.enrollAs(excelRow).clickContinue().clickContinue().fillProviderOrgInfo().fillPrimaryProvInfo().clickContinue().fillFinancialInstInfo().clickContinue().uploadW9().fillInfoAndClickSubmit().validateEnrollmentInfo();
-	 }
-	
-	@Test(priority=4,description="US1202167--TS003 Validates User Enrollment As BS")
-	public void testUserEnrollmentAsBS() throws IOException
-	 {
-		String option="Health plan communication";
-		int excelRow=2;
-		UPARegistrationPage registrationPage = new UPARegistrationPage(testConfig);
-		BeginEnrollment beginEnrollmentPage=  registrationPage.clickEnrollNow();
-		BeginEnrollmentContinue beginEnrollmentContinuePage= beginEnrollmentPage.selectHowYouHeard(option);
-		beginEnrollmentContinuePage.enrollAs(excelRow).clickContinue().clickContinue().fillProviderOrgInfo().fillPrimaryProvInfo().clickContinueToW9().uploadW9().fillInfoAndClickSubmit().validateEnrollmentInfo();
-	 }
 	
 	@Test(priority=4,description="US1202167--TS004/TS005/TS006/TS015 Validates what shall i choose after Selecting Health Org /TS005 Validates Pop Up after clicking Close link")
 	public void testPopUpAfterChoosingHealthOrg() throws IOException
@@ -92,28 +66,9 @@ public class TestCreateEnrollment extends TestBase{
 		beginEnrollmentContinuePage.verifyPopUp("VO");
 	 } 
 	
-	@Test(priority=4,description="US1202167--TS010 Validates navigation upon selection of I agree option on VCP pop up ")
-	public void testEnrollAsVCP() throws IOException
-	 {
-		String option="Health plan communication";
-		int excelRowNo=4;
-		UPARegistrationPage registrationPage = new UPARegistrationPage(testConfig);
-		BeginEnrollment beginEnrollmentPage=  registrationPage.clickEnrollNow();
-		BeginEnrollmentContinue beginEnrollmentContinuePage= beginEnrollmentPage.selectHowYouHeard(option);
-		beginEnrollmentContinuePage.enrollAs(excelRowNo).clickContinue().clickContinue().fillProviderOrgInfo().fillPrimaryProvInfo().clickContinueToW9().uploadW9().fillInfoAndClickSubmit().validateEnrollmentInfo();
-		
-	 }
+
 	
-	@Test(priority=4,description="US1202167--TS011/TS014 Validates navigation upon selection of I agree option on ACH and VCP pop up")
-	public void testEnrollAsVCPandACH() throws IOException
-	 {
-		String option="Health plan communication";
-		int excelRowNo=3;
-		UPARegistrationPage registrationPage = new UPARegistrationPage(testConfig);
-		BeginEnrollment beginEnrollmentPage=  registrationPage.clickEnrollNow();
-		BeginEnrollmentContinue beginEnrollmentContinuePage= beginEnrollmentPage.selectHowYouHeard(option);
-		beginEnrollmentContinuePage.enrollAs(excelRowNo).clickContinue().clickContinue().fillProviderOrgInfo().fillPrimaryProvInfo().clickContinue().fillFinancialInstInfo().clickContinueAV().clickContinue().uploadW9().fillInfoAndClickSubmit().validateEnrollmentInfo();	
-	 }
+	
 
 	@Test(priority=4,description="US1202167--TS012/TS013 Validates navigation upon clicking Cancel on VCP PopUp Box")
 	public void testChooseVCPandACH() throws IOException
