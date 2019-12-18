a9b576eac01e5bf880510febb788cc02f660384d
Madhavi, Upputuri <madhavi_upputuri@optum.com>
2019-12-18 12:25:23 +0530

    ReviewAndSubmit Step File change
    ReviewAndSubmit Step File change

--- Online-Portals/UPA/src/main/java/stepDefinitions/CrtEnrolmnt/ReviewAndSubmitSteps.java
+++ Online-Portals/UPA/src/main/java/stepDefinitions/CrtEnrolmnt/ReviewAndSubmitSteps.java
@@ -6,7 +6,7 @@ import main.java.pageObjects.EnrollmentSubmitted;
 import main.java.pageObjects.HeaderContentValidation;
 import main.java.pageObjects.ReviewAndSubmit;
 
-public class ProviderHOReviewSubmitSteps extends TestBase {
+public class ReviewAndSubmitSteps extends TestBase {
 	
 	ReviewAndSubmit reviewAndSubmit=null;
 	EnrollmentSubmitted enrollmentSubmitted=null;
