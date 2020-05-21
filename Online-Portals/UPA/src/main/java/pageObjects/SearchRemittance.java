3cc4ac79de8a8e44e1e273d7eef7ecb7113d592a
Krishna <rahul_krishna38@optum.com>
2020-05-21 18:12:31 +0530

    Update SearchRemittance.java

--- Online-Portals/UPA/src/main/java/pageObjects/SearchRemittance.java
+++ Online-Portals/UPA/src/main/java/pageObjects/SearchRemittance.java
@@ -1391,8 +1391,8 @@ public class SearchRemittance extends paymentSummary {
 			    		  lnkEpraPdf=Element.findElement(testConfig, "xpath", "//form[@id='paymentsummaryform']/table[1]/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr["+(i+1)+"]/td[11]/table/tbody/tr/td[3]/span/span/a/img");                  
 					  
 					 Browser.wait(testConfig, 5);
-					 Browser.browserRefresh(testConfig);
-					 Browser.wait(testConfig, 3);
+					// Browser.browserRefresh(testConfig);
+					// Browser.wait(testConfig, 3);
 					 if(srchType.equals("viewPayments"))
 					 Element.waitForPresenceOfElementLocated(testConfig, By.xpath("//form[@id='paymentsummaryform']/table[1]/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr["+(i+1)+"]/td[11]/table/tbody/tr/td[3]/span/span/a/img"), 60);
 					 Element.verifyElementPresent(lnkEpraPdf, "EPRA pdf icon");
