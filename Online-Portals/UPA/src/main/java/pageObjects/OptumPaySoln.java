bddd74fa24c8e75e7c74c67c4945e1a4f192298e
tathyush <athyusha.thota1@optum.com>
2021-02-03 18:39:24 +0530

    Commiting Sayonee's Code

--- Online-Portals/UPA/src/main/java/pageObjects/OptumPaySoln.java
+++ Online-Portals/UPA/src/main/java/pageObjects/OptumPaySoln.java
@@ -99,5 +99,95 @@ public class OptumPaySoln {
 		return this;
 	
 	}
+	public void validatePlanTypeTile() throws ParseException  {
+		int sqlRowNo=1509;
+		testConfig.getRunTimeProperty("tin");
+		Map portalUserData = DataBase.executeSelectQuery(testConfig,sqlRowNo, 1);
+		
+		if(portalUserData.get("PRTL_PRDCT_SELECT_DTTM").toString().length()>0)
+		{String activationDate= portalUserData.get("PRTL_PRDCT_SELECT_DTTM").toString().trim().substring(0, 10).trim();
+		 activationDate=Helper.changeDateFormat(activationDate, "yyyy-mm-dd", "mm/dd/yyyy");
+			Helper.compareContains(testConfig, "Plan Type Tile text- Activation Date", activationDate, tilePlanType.getText().trim());
+		}
+		Helper.compareContains(testConfig, "Plan Type Tile text- Optum Pay", "Optum Pay", tilePlanType.getText().trim());	
+		Helper.compareContains(testConfig, "Plan Type Tile text- Billing Cycle", "Billing Cycle: Monthly", tilePlanType.getText().trim());
+		Helper.compareContains(testConfig, "Plan Type Tile text- Activation Admin", "Activation Admin:", tilePlanType.getText().trim());
+        Helper.compareContains(testConfig, "Plan Type Tile text- Activation Date", "Activation Date:", tilePlanType.getText().trim());
+        Helper.compareContains(testConfig, "Plan Type Tile text- Activation name", portalUserData.get("PRTL_PRDCT_SELECT_USER_FULLNAME").toString().trim(), tilePlanType.getText().trim());
+
+		Element.clickByJS(testConfig,lnkLogOut , "logging Out of the portal");
+	}
+		public void validateChangeRatePopup(String credentials) {
+		
+		if(credentials.equalsIgnoreCase("Super"))
+		{
+		Element.clickByJS(testConfig, lnkChangeRate, "Change Rate link");
+		Element.clickByJS(testConfig,optionReasonForRateChange,"Rate Change Reason");
+		Element.clickByJS(testConfig,btnSaveChangeRate,"Save Rate Change");
+		System.out.println(popUpChangeRate.getText());
+		Helper.compareContains(testConfig, "PopUp text", "Are you sure?" ,popUpChangeRate.getText().trim());
+		Helper.compareContains(testConfig, "PopUp text", "If you proceed with this rate change the new per payment rate for this" ,popUpChangeRate.getText().trim());
+		Helper.compareContains(testConfig, "PopUp text", "TIN will be effective starting the next business day" ,popUpChangeRate.getText().trim());
+			
+        Element.verifyElementPresent(btnChangeRatePopupChange, "change btn");
+        Element.verifyElementPresent(btnChangeRatePopupCancel, "cancel btn");
+        Element.clickByJS(testConfig, btnChangeRatePopupCancel, "cancel btn");
+		}
+		else
+			Element.verifyElementNotPresent(lnkChangeRate, "Change Rate link");
+		
+		Element.clickByJS(testConfig,lnkLogOut , "logging Out of the portal");
+	}
+		
+		public void validateInfoIconHover() {
+			for(WebElement title: titles)
+			  Element.mouseHoverByJS(testConfig, title, "title");
+	 
+			Helper.compareEquals(testConfig, "Plan Type","Providers will be billed monthly\n" + 
+					"for any fees incurred the previous\n" + 
+					"month. For example, fees accrued\n" + 
+					"during the month of June will be\n" + 
+					"invoiced within the first 5\n" + 
+					"business days of July. Provider\n" + 
+					"administrators will receive an\n" + 
+					"email in advance of the debit to\n" + 
+					"the TIN-level bank account and\n" + 
+					"they can review the fees on the\n" + 
+					"Invoices subtab.", hoverPlanType.getText().trim());
+			Helper.compareEquals(testConfig, "Plan Type", "Per payment fees are\n" + 
+					"calculated based on the\n" + 
+					"total payment amount.\n" + 
+					"Any rate changes will be\n" + 
+					"effective the following\n" + 
+					"business day.",hoverRate.getText().trim());
+			Helper.compareEquals(testConfig, "Plan Type", "To view individual per-\n" + 
+					"payment fees, please visit\n" + 
+					"the View Payments page.\n" + 
+					"Fees will be billed monthly.\n" + 
+					"To estimate monthly fees,\n" + 
+					"select the Print Payment\n" + 
+					"summary button from the\n" + 
+					"View Payments page to\n" + 
+					"download 30 days of\n" + 
+					"payment data. Then,\n" + 
+					"calculate that amount by\n" + 
+					"the current rate.", hoverFees.getText().trim());
+			Helper.compareEquals(testConfig, "Plan Type", "To cancel the paid subscription,\n" + 
+					"Provider administrators\n" + 
+					"complete the Cancellation Fee\n" + 
+					"Form found in the Resources link\n" + 
+					"and email it to\n" + 
+					"optumpay_cancel@optum.com.\n" + 
+					"\n" + 
+					"Note: Cancellation may take up\n" + 
+					"to 7 days to process during which\n" + 
+					"time the provider will be\n" + 
+					"responsible for any charges to\n" + 
+					"their account. Any fees incurred\n" + 
+					"prior to cancellation will be billed\n" + 
+					"at the end of the current billing\n" + 
+					"cycle.",hoverManageMyPlan.getText().trim());
+		
+		}
 	
 }
--- Online-Portals/UPA/src/test/java/Features/OptumPaySoln/CSR_OptumPay_Solutions_Premium.feature
+++ Online-Portals/UPA/src/test/java/Features/OptumPaySoln/CSR_OptumPay_Solutions_Premium.feature
@@ -32,4 +32,46 @@
          |    Super          |		withoutAccuredFee	|
          |    RW	           |		withoutAccuredFee	|
          |    RO   		       |		withoutAccuredFee	|
-             
+   
+   #Author: Sayonee
+    @CSRUS3136567
+    Scenario Outline: Optum Pay Solutions Plan Type Tile Content for Premium Tins
+		Given User navigates to CSR portal and enters "<credentials>" and login
+		Then User clicks on Optum Pay Soultions link on CSR HomePage
+		Then User enters "<tinTyp>" TIN with "<portalAccess>" and click on Search button
+		Then User validates contents of Plan Type tile of this page 
+     Examples:
+
+         |   credentials     |  tinTyp  |portalAccess|
+         |    Super          |	AO			|  Premium	 |
+         |    RW   		       |	AO			|  Premium	 |
+         |    RO	           |	AO			|  Premium	 |       
+         
+    @CSRUS3138920
+    Scenario Outline: Optum Pay Solutions Plan Type Tile Content for Premium Tins
+		Given User navigates to CSR portal and enters "<credentials>" and login
+		Then User clicks on Optum Pay Soultions link on CSR HomePage
+		Then User enters "<tinTyp>" TIN with "<portalAccess>" and click on Search button
+		Then User then validates the Change Rate popup based "<credentials>"
+     Examples:
+
+         |   credentials     |  tinTyp  |portalAccess|
+         |    Super          |	AO			|  Premium	 |
+         |    RW   		       |	AO			|  Premium	 |
+         |    RO	           |	AO			|  Premium	 |
+    @CSRUS3157067
+    
+    Scenario Outline: Optum Pay Solutions Plan Type Tile Content for Premium Tins
+		Given User navigates to CSR portal and enters "<credentials>" and login
+		Then User clicks on Optum Pay Soultions link on CSR HomePage
+		Then User enters "<tinTyp>" TIN with "<portalAccess>" and click on Search button
+		Then User validates the hover on info icon on the tiles 
+     Examples:
+
+         |   credentials     |  tinTyp  |portalAccess|
+         |    Super          |	AO			|  Premium	 |
+         |    RW   		       |	AO			|  Premium	 |
+         |    RO	           |	AO			|  Premium	 |            
+         
+         
+                      
