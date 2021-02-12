a59d03b823be5c85924470cbb5f6325f3107938e
Sayonee <dassani_sayonee@optum.com>
2021-02-12 22:21:03 +0530

    ..

--- Online-Portals/UPA/src/main/java/pageObjects/CSRHomePage.java
+++ Online-Portals/UPA/src/main/java/pageObjects/CSRHomePage.java
@@ -106,6 +106,9 @@ public class CSRHomePage {
 	@FindBy(linkText="Manage Internal Users")
 	WebElement lnkManageInternalUSer;
 	
+	@FindBy(linkText="Run Reports")
+    WebElement linkRunReports;
+	
 	CSRHomePage(TestBase testConfig) 
 	{
 		this.testConfig=testConfig;
@@ -171,7 +174,11 @@ public class CSRHomePage {
     }
     public void clickManageInternalUserlink() {
 		Element.clickByJS(testConfig, lnkManageInternalUSer, "Manage Internal Users Link");
-		
-		
+				
+	}
+    public void clickRunReportLink() {
+    	Element.clickByJS(testConfig,linkRunReports,"CLick the RunReport link");
+    	Browser.wait(testConfig, 1);
+     
 	}
 }
