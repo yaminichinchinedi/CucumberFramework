6b53641ffa91306d33982570bd5e7c219b8b088b
Sunanda, Ganti <sunanda_ganti@optum.com>
2021-05-18 16:07:18 +0530

    page texts update

--- Online-Portals/UPA/src/main/java/stepDefinitions/PaymentDataFiles/SearchTinPagePaymentDataFilesSteps.java
+++ Online-Portals/UPA/src/main/java/stepDefinitions/PaymentDataFiles/SearchTinPagePaymentDataFilesSteps.java
@@ -21,9 +21,6 @@ public void User_Validates_Error_Message_upon_Entering_invalid_TIN() throws Thro
 	srchTinPagePaymentDataFiles.verifyErrorMsgForInvalidTIN();
 }
 
-	@Then("^Validate Payment Data File Page Text for \"([^\"]*)\" for \"([^\"]*)\"$")
-	public void validatePageTextForForFor(String credentials, String portalAccess) {
-		srchTinPagePaymentDataFiles.validatePageText(credentials, portalAccess);
-	}
+
 
 }
